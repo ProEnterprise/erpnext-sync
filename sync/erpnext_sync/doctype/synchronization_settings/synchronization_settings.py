47c7220218a29db86dce69d3c3f6906863b0f698
Chris Ian Fiel <ccfiel@gmail.com>
2017-08-06 12:04:00 +0800

    connect to master server

--- sync/erpnext_sync/doctype/synchronization_settings/synchronization_settings.py
+++ sync/erpnext_sync/doctype/synchronization_settings/synchronization_settings.py
@@ -5,6 +5,24 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from sync.frappeclient import FrappeClient
+from frappe.utils.password import get_decrypted_password
+
 
 class SynchronizationSettings(Document):
-	pass
+
+    def validate(self):
+        if self.sync_mode == 'Slave':
+            if not self.master_url or not self.username or not self.password:
+                frappe.throw('URL, username and password should have a value')
+
+            password = get_decrypted_password(self.doctype, self.doctype, 'password', False)
+
+            try:
+                FrappeClient(self.master_url, self.username, password)
+            except:
+                frappe.throw('Error connecting server')
+
+
+
+
