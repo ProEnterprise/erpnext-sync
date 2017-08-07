3bba7d1498841006a4eb0668c667e9367bae6afd
Chris Ian Fiel <ccfiel@gmail.com>
2017-08-07 08:20:03 +0800

    if slave doctype sync not allow to edit/add

--- sync/rest.py
+++ sync/rest.py
@@ -5,9 +5,11 @@ record_per_page = 200
 
 @frappe.whitelist(allow_guest=True)
 def download_data(doc_name, date, page):
-    fields = frappe.db.sql('''DESCRIBE %s''', doc_name)
-    count = frappe.db.sql('''SELECT count(*) from %s WHERE creation>=%s and modified>=%s''', (doc_name, date, date))
-    data = frappe.db.sql('''SELECT * from %s WHERE creation>=%s and modified>=%s LIMIT %s OFFSET %s''',
-                         (doc_name, date, date, record_per_page, page * count[0][0]))
+    fields = frappe.db.sql('DESCRIBE `' + doc_name + '`')
+    count = frappe.db.sql('SELECT count(*) from `' + doc_name + '` WHERE creation>=%s and modified>=%s', (date, date))
+    start = (int(page) - 1) * record_per_page
+    data = frappe.db.sql('SELECT * from `' + doc_name +
+                         '` WHERE creation>=%s and modified>=%s LIMIT ' +
+                         str(record_per_page) + ' OFFSET ' + str(start), (date, date))
 
     return {"fields": fields, "data": data, "count": count[0][0]}
